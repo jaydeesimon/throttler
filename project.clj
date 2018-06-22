7e5aa0948d87b3d9b5184680f1c089aeda7af913
Jeffrey Simon <jeffrey.simon@greenhouse.io>
2018-06-21 22:18:20 -0400

    upgrade dependencies; add deps.edn

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/brunoV/throttler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]]
+  :profiles {:dev {:dependencies [[midje "1.9.1"]
                                   [criterium "0.4.3"]]
-                   :plugins [[lein-midje "3.1.1"]]}})
+                   :plugins [[lein-midje "3.2.1"]]}})
